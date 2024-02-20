0f0f3b9634a9023024d8ca5f3a7328a43aa8486a
Albert Hembd <alhembd@gmail.com>
2024-02-19 22:01:00 -0600

    this is the valid file.

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
-from time import sleep
+
 
 @given('Google Chrome is open and the user has navigated to the Target webpage')
 def open_target(context):
     context.driver.get('https://www.target.com/')
+
 
 @when('the user clicks on the cart icon without having put any items into it')
 def click_on_the_cart_icon(context):
@@ -13,7 +14,7 @@ def click_on_the_cart_icon(context):
 
 @then('Your cart is empty message appears')
 def verify_cart_is_empty(context):
-    actual_text = context.driver.find_element(By.CSS_SELECTOR, "h1[class*=‘StyleHeading’]").text
+    actual_text = context.driver.find_element(By.CSS_SELECTOR, "[data-test='boxEmptyMsg']").text
     print(actual_text)
     expected_result = 'Your cart is empty'
     assert expected_result in actual_text, f'Expected result {expected_result} + not in {actual_text}'
