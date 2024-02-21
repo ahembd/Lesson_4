f4984bf03b436c9e3ef5c3292b4ba294f37da218
Albert J Hembd <42681737+ahembd@users.noreply.github.com>
2024-02-21 17:00:46 -0600

    Corrected code

--- steps/steps.py
+++ steps/steps.py
@@ -1,23 +1,21 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
-from selenium.common.exceptions import NoSuchElementException
-from time import sleep
+
 
 @given('Google Chrome is open and the user has navigated to the Target webpage')
 def open_target(context):
     context.driver.get('https://www.target.com/')
 
+
 @when('the user clicks on the cart icon without having put any items into it')
 def click_on_the_cart_icon(context):
-    context.driver.find_element(By.ID, '//*[@id="headerPrimary"]/a[5]').click()
+    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartIcon']").click()
 
-xpath = '//*[@id="headerPrimary"]/a[5]'
 
-@then('the "Your cart is empty" message appears')
-def check_exists_by_xpath(context):
-    try:
-        context.driver.find_element(By.XPATH, xpath)
-    except NoSuchElementException:
-        return False
-    return True
+@then('Your cart is empty message appears')
+def verify_cart_is_empty(context):
+    actual_text = context.driver.find_element(By.CSS_SELECTOR, "[data-test='boxEmptyMsg']").text
+    print(actual_text)
+    expected_result = 'Your cart is empty'
+    assert expected_result in actual_text, f'Expected result {expected_result} + not in {actual_text}'
 
